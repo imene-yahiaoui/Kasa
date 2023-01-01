8541f889700d211165d6ae1e38a5617b176aa428
imene ikhlef <imeneikhlefyahiaoui@users.noreply.github.com>
2023-01-02 00:45:53 +0100

    Update README.md

--- src/components/RoutesPath/RoutesPath.jsx
+++ src/components/RoutesPath/RoutesPath.jsx
@@ -1,16 +1,15 @@
-import { Routes, Route,Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../home/Home";
 import Housing from "../housing/Housing";
 import About from "../about/About";
 import NotFound from "../notFound/NotFound";
 // import Test from "../housing/test"
 
-const RoutesPath = ({ posts , dataAbout }) => {
+const RoutesPath = ({ posts, dataAbout }) => {
   return (
     <div>
       <Routes>
         <Route path="/" exact element={<Home posts={posts} />} />
-        <Route path="/kaza" element={<Navigate to="/" replace posts={posts}/>} />
         <Route path="/Housing/:id" element={<Housing posts={posts} />} />
         <Route path="/About" element={<About dataAbout={dataAbout} />} />
         <Route path="/*" element={<NotFound />} />
